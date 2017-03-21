30c091c1daf9b68986e9a9233264c93aec8443c7
Justin Bellero <jpbellero@gmail.com>
2017-03-21 13:41:27 -0400

    adds twilio keys

--- config/auth.js
+++ config/auth.js
@@ -1,3 +1,5 @@
 module.exports = {
-  authyApiKey: '388AYCdKbcu7shNi7t93msg0OEo6sVGL'
+  authyApiKey: '388AYCdKbcu7shNi7t93msg0OEo6sVGL',
+  token: '72c284050fe976bbaa1ddd40b8632c78',
+  sid: 'ACfd08d4399295e8cdb7d2936078f00660'
 };
