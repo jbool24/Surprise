dcc5623117d4a886b76e9db074a64cd8867e9c75
ptberhane <piniel.berhane@gmail.com>
2017-03-21 20:51:27 -0400

    adding frontend

---
+++ models/userevents.js
@@ -0,0 +1,11 @@
+/*'use strict';
+//use umodel export to 
+module.exports = function(sequelize, DataType){
+	//create User model
+
+	//var Users = sequelize.define("Users", {
+		//this model needs: 
+
+	//'return' the post after defining
+ return Users;
+};*/
