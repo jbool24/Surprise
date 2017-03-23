b6a4a825d9ca36e0ff989012577623d0d90f0621
ptberhane <piniel.berhane@gmail.com>
2017-03-23 04:04:41 -0400

    image and create route

---
+++ routes/create.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var exphbs = require("express-handlebars");
+/* GET create page. */
+
+var creation = require("../models/users.js");
+router.get('/:create', function(req, res, next) {
+  res.render('create', {body});
+});
+
+module.exports = router;
