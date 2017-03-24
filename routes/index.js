f0eca41a9ecb8dde42c00077ef75ac47cff47aeb
Alejandra Agreda <agos.alejandramaria@gmail.com>
2017-03-23 21:22:10 -0400

    routes

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var exphbs = require("express-handlebars");
+var login = require("../models/session.js");
+
+/* GET home page. whihc is the login page */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+
+});
+
+module.exports = router;
