192962311e6e20440909cc5446fdadf00911cdc9
Justin Bellero <jpbellero@gmail.com>
2017-03-24 23:28:32 -0400

    updates

--- schema.sql
+++ schema.sql
@@ -4,8 +4,6 @@ USE `surprise_db`;
 --
 -- Database: surprise_db
 -- ------------------------------------------------------
-
-
 --
 -- Table structure for table `Events`
 --
@@ -32,7 +30,9 @@ CREATE TABLE `Events` (
 
 LOCK TABLES `Events` WRITE;
 /*!40000 ALTER TABLE `Events` DISABLE KEYS */;
-INSERT INTO `Events` VALUES (1,'happy hour','2017-03-21 23:00:44',30,'Half hour of free beer\n','2017-03-21 23:09:44','2017-03-21 23:09:44',0),(2,'sadhour','2017-03-21 23:09:44',30,'Double priced shots','2017-03-21 23:09:44','2017-03-21 23:09:44',0);
+INSERT INTO `Events` 
+VALUES (1,'happy hour','2017-03-25 12:30:44',30,'Half hour of free beer\n','2017-03-21 23:09:44','2017-03-21 23:09:44',0),
+(2,'sadhour','2017-03-21 1:09:44',30,'Double priced shots','2017-03-21 23:09:44','2017-03-21 23:09:44',0);
 /*!40000 ALTER TABLE `Events` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,15 +54,6 @@ CREATE TABLE `Sessions` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `Sessions`
---
-
-LOCK TABLES `Sessions` WRITE;
-/*!40000 ALTER TABLE `Sessions` DISABLE KEYS */;
-INSERT INTO `Sessions` VALUES (1,'1','788318f0-0e8b-11e7-b928-6fd478ca9db7',1,'2017-03-21 23:09:44','2017-03-21 23:09:44'),(2,'2','6a880fe0-0e94-11e7-8511-c333b58efd80',1,'2017-03-22 00:13:46','2017-03-22 00:13:46');
-/*!40000 ALTER TABLE `Sessions` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `UserEvents`
@@ -89,7 +80,10 @@ CREATE TABLE `UserEvents` (
 
 LOCK TABLES `UserEvents` WRITE;
 /*!40000 ALTER TABLE `UserEvents` DISABLE KEYS */;
-INSERT INTO `UserEvents` VALUES ('2017-03-21 23:09:44','2017-03-21 23:09:44',1,1),('2017-03-21 23:09:44','2017-03-21 23:09:44',1,2),('2017-03-21 23:09:44','2017-03-21 23:09:44',2,1);
+INSERT INTO `UserEvents` VALUES
+('2017-03-21 23:09:44','2017-03-21 23:09:44',1,1),
+('2017-03-21 23:09:44','2017-03-21 23:09:44',1,2),
+('2017-03-21 23:09:44','2017-03-21 23:09:44',2,1);
 /*!40000 ALTER TABLE `UserEvents` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,7 +115,9 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (1,'Justin','jpbellero@gmail.com','+1','9084562367','36606015',NULL,'$2a$10$VI9TLE74VdfHFDh1V/.Cauq1vLEbj.MBQnC9zxql/qx9ZQpfoQugS','2017-03-21 23:09:44','2017-03-21 23:09:44'),(2,'Kevin','kevinrlawler@gmail.com','+1','8472266734','36925943',NULL,'$2a$10$G5AeX6e3t.IJVrKCR8VfHeA3RRSB5U.CEPoJAhDskgjD3DUsK8c6q','2017-03-22 00:13:46','2017-03-22 00:13:46');
+INSERT INTO `Users` VALUES 
+(1,'Justin','jpbellero@gmail.com','+1','9084562367','36606015',NULL,'$2a$10$VI9TLE74VdfHFDh1V/.Cauq1vLEbj.MBQnC9zxql/qx9ZQpfoQugS','2017-03-21 23:09:44','2017-03-21 23:09:44'),
+(2,'Kevin','kevinrlawler@gmail.com','+1','8472266734','36925943',NULL,'$2a$10$G5AeX6e3t.IJVrKCR8VfHeA3RRSB5U.CEPoJAhDskgjD3DUsK8c6q','2017-03-22 00:13:46','2017-03-22 00:13:46');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
